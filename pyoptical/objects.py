83c75b342e53999d76f12e094ec0e2a9708a78b3
artemisbeta <KXZ186@student.bham.ac.uk>
2017-12-20 10:54:12 +0000

    Fixed infinite lens height

--- pyoptical/objects.py
+++ pyoptical/objects.py
@@ -2,31 +2,37 @@ from pyoptical.light_ray import lightray
 
 class line_object:
 	def __init__(self, height, position, lens_list):
-		self._height = height
-		self._position = position
+		self.height = height
+		self.position = position
 		self._light_rays = []
 		for lens in lens_list:
 			_ray = lightray((position, height))
 			self.add_light_ray(_ray, lens, 0)
 			_ray = lightray((position, height))
 			from math import pi
-			self.add_light_ray(_ray, lens, 0.1)
+			self.add_light_ray(_ray, lens, 1.1)
 			_ray = lightray((position, height))
-			self.add_light_ray(_ray, lens, -pi/8.)
+			self.add_light_ray(_ray, lens, -1.1)
 
 	def add_light_ray(self, ray, lens, angle):
 		assert isinstance(ray, lightray), "Object not of type 'lightray'"
 		self._light_rays.append(ray)
 		_x = lens._position[0]
 		from math import tan
-		_y = (self._position-_x)/tan(-angle) if angle != 0 else self._height
-		_y2 = self._position/tan(-angle) if angle != 0 else self._height
+		_y = (self.position-_x)/tan(angle) if angle != 0 else self.height
+		_y2 = self.position/tan(angle) if angle != 0 else self.height
 		self._light_rays[-1].add_node((_x, _y))
-		self._light_rays[-1].add_node(lens.get_after_lens((-self._position, _y2)))
+		if _y2 > -lens._height/2. and _y2 < lens._height/2.:
+			self._light_rays[-1].add_node(lens.get_after_lens((-self.position, _y2)))
+		else:
+			_gradient = (_x-self.position)/(_y2+self.height)
+			_inter = _y2-_gradient*_x
+			print(_x, self.position, _y2, self.height, _gradient, _inter)
+			self._light_rays[-1].add_node((-self.position, -self.position*_gradient+_inter))
 
 	def show_on(self, ax):
 		from matplotlib.patches import Arrow
-		_object = Arrow(self._position, 0, 0, self._height, width=self._height*0.1)
+		_object = Arrow(self.position, 0, 0, self.height, width=self.height*0.1)
 		ax.add_patch(_object)
 		for ray in self._light_rays:
 			ray.show_on(ax)
